b9cd0253ac9ef4a5efdfae8f2f39e93f65b3e86c
Afridi Rahman Bondhon <afridirb@gmail.com>
2025-05-30 01:22:41 +0600

    fifth commit

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,6 +1,15 @@
 "use client"
-import type { ThemeProviderProps } from "next-themes"
+
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import type * as React from "react"
+
+interface ThemeProviderProps {
+  children: React.ReactNode
+  attribute?: string
+  defaultTheme?: string
+  enableSystem?: boolean
+  disableTransitionOnChange?: boolean
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
